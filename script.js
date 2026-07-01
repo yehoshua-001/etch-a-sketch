64a8bee08b1cbbc6671117a5bb8020da9be59755
yehoshua-001 <joshuafelicilda248@gmail.com>
2026-07-01 13:23:17 +0800

    Edit script.js
    Added createGrid() with default grid size (16x16), changed the loop for
    squares and moved it into function, added addEventListener whenever
    mouse enters the divs, and another addEventListener for the button with
    prompt, conditionals to check the input, and calls back the createGrid().

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const container = document.querySelector('#container');
-const squares = 16 * 16;
 
-for(i = 0; i < squares; i++){
-    const content = document.createElement('div');
-    content.classList.add('squares');
-    container.appendChild(content);
+function createGrid(size){
+    container.replaceChildren();
+    const totalSquares = size * size;
+    const squareSize = 100 / size;
+    for(i = 0; i < totalSquares; i++){
+        const squares = document.createElement('div');
+        squares.classList.add('squares');
+        squares.style.flexBasis = `${squareSize}%`;
+        squares.style.height = `${squareSize}%`
+        container.appendChild(squares);
+    }
+    const squares = document.querySelectorAll('.squares');
+    squares.forEach(square => {
+        square.addEventListener('mouseenter', (e) => {
+            e.target.style.backgroundColor = '#000000';
+        });    
+    });    
 }
+createGrid(16);
+
+const gridSizeBtn = document.querySelector('#gridSizeBtn');
+gridSizeBtn.addEventListener('click', () => {
+    const newSize = Number(prompt('Enter a number'));
+    if(newSize >= 1 && newSize <=100){
+        createGrid(newSize);
+    }
+    else{
+        alert('Invalid size. Please enter a size between 1 and 100.')
+    }
+});
